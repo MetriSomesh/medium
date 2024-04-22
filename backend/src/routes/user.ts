dae4b229d47d4709202bd9308777c9dc61db0ff3
MetriSomesh <someshmetri44@gmail.com>
2024-04-22 19:39:27 +0530

    added backend

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,83 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+import { signinInput, signupInput } from "@someshmetri/medium-common";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const body = await c.req.json();
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs are not Correct",
+    });
+  }
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const token = "Bearer " + (await sign({ id: user.id }, c.env.JWT_SECRET));
+
+    return c.json({
+      jwt: token,
+    });
+  } catch (e) {
+    return c.status(403);
+  }
+});
+
+userRouter.post("signin", async (c) => {
+  const body = await c.req.json();
+  const { success } = signinInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs are not Correct",
+    });
+  }
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.json({
+        error: "User not found",
+      });
+    }
+
+    const jwt = "Bearer " + (await sign({ id: user.id }, c.env.JWT_SECRET));
+    return c.json({ jwt });
+  } catch (error) {
+    c.status(500);
+    c.json({
+      error: "Something went wrong",
+    });
+  }
+});
